308460dfa46120b5bc987f9cf05156a579fda34f
Miguel Angel <m.escobar_27@outlook.com>
2019-04-26 14:12:52 -0400

    persistencia y conexion a la base de datos

---
+++ Academico.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Academico.Web.Data
+{
+    using Academico.Web.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+        }
+
+    }
+
+}
---
+++ Academico.Web/Data/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace Academico.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;    
+
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Image") ]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Is Available?")]
+        public bool IsAvailable { get; set; }
+
+    }
+}
