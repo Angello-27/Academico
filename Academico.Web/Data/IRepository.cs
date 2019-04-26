923374f63af0e2934097983272628daadc813fe8
Miguel Angel <m.escobar_27@outlook.com>
2019-04-26 18:06:33 -0400

    guardando cambios del dia

---
+++ Academico.Web/Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Academico.Web.Data
+{
+    using Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepository
+    {
+        void AddStudent(Student student);
+
+        Student GetStudent(int id);
+
+        IEnumerable<Student> GetStudents();
+
+        void RemoveStudent(Student student);
+
+        Task<bool> SaveAllAsync();
+
+        bool StudentExists(int id);
+
+        void UpdateStudent(Student student);
+    }
+}
--- Academico.Web/Data/Repository.cs
+++ Academico.Web/Data/Repository.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    public class Repository
+    public class Repository : IRepository
     {
         private readonly DataContext context;
 
