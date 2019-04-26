a09b78971657ac25f4d7eb0c1c550b25834f8aeb
Miguel Angel <m.escobar_27@outlook.com>
2019-04-26 11:58:14 -0400

    Estructura de la solucion del proyecto

---
+++ Academico.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Academico.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Academico.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Academico.UIClassic
+{
+    public class Class1
+    {
+    }
+}
