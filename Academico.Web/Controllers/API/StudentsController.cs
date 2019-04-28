604ff78c76d5579788c551e526d868ee3a8487cc
Miguel Angel Escobar <M.escobar_27@outlook.com>
2019-04-28 13:17:45 -0400

    Api Controller Student

---
+++ Academico.Web/Controllers/API/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Academico.Web.Controllers.API
+{
+    using Academico.Web.Data.Repository;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository stundentRepository;
+
+        public StudentsController(IStudentRepository stundentRepository)
+        {
+            this.stundentRepository = stundentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(this.stundentRepository.GetAll());
+        }
+    }
+}
