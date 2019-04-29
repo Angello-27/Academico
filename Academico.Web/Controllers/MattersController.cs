d50d14aff4f38ff8fa775142008c3987d14103af
Miguel Angel <m.escobar_27@outlook.com>
2019-04-29 11:13:12 -0400

    Prueba usando las materias

---
+++ Academico.Web/Controllers/MattersController.cs
@@ -0,0 +1,23 @@
+﻿namespace Academico.Web.Controllers
+{
+    using Academico.Web.Data.Repository;
+    using Academico.Web.Hepers;
+    using Microsoft.AspNetCore.Mvc;
+    public class MattersController : Controller
+    {
+        private readonly IMatterRepository repository;
+        private readonly IUserHelper userHelper;
+
+        public MattersController(IMatterRepository repository, IUserHelper userHelper)
+        {
+            this.repository = repository;
+            this.userHelper = userHelper;
+        }
+
+        // GET: Students
+        public IActionResult Index()
+        {
+            return View(this.repository.GetAll());
+        }
+    }
+}
